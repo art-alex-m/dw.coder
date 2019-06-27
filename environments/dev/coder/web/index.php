31b3b5dfa9507f0e2c0e3ee0bc82c9200725a2c5
Alexander M. Artamonov <art.alex.m@gmail.com>
2019-06-27 15:41:38 +0300

    DWC #dw.coder-1: Развернуть скелет проекта на Yii2 Docker. Рефакторинг структуры

---
+++ environments/dev/coder/web/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * index.php
+ *
+ * Created by PhpStorm.
+ * @date 27.06.19
+ * @time 15:15
+ */
+
+echo '<h1>Yes it works!</h1>';
--- environments/index.php
+++ environments/index.php
@@ -32,37 +32,18 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
             'console/runtime',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'www/runtime',
+            'www/web/assets',
         ],
         'setExecutable' => [
             'yii',
             'yii_test',
+            'coder/yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
             'common/config/codeception-local.php',
-            'frontend/config/main-local.php',
-        ],
-    ],
-    'Production' => [
-        'path' => 'prod',
-        'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'console/runtime',
-            'frontend/runtime',
-            'frontend/web/assets',
-        ],
-        'setExecutable' => [
-            'yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'www/config/main-local.php',
         ],
     ],
 ];
